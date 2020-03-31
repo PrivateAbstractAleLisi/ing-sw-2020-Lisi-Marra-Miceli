a653f4f8fd5967e226590078ba773b6caec7ab1c
beat-axo <55945432+beat-axo@users.noreply.github.com>
2020-03-31 23:12:02 +0200

    Minor Updates to Player

--- SantoriniOnline/src/main/java/model/Player.java
+++ SantoriniOnline/src/main/java/model/Player.java
@@ -1,51 +1,60 @@
 package model;
 
 import java.util.ArrayList;
+
 import model.gamemap.Worker;
 import model.Card;
 
 public class Player {
+
     private String username;
     private ArrayList<Worker> workersList = new ArrayList<Worker>();
     private WorkerColors color;
     private Card card;
-    protected GameManager match;
-    protected BehaviourManager behaviour;
+
+    private BehaviourManager behaviour;
 
     /**
      * Constructor of the player class
+     *
      * @param username the name of the player
+     * @param creator
      */
-    public Player(String username) {
-        this.username= username;
+    public Player(String username, GameManager creator) {
+        this.username = username;
+        behaviour = new BehaviourManager();
     }
 
+
     /**
-     *
      * @return the username of the player
      */
-    public String getUsername(){
+    public String getUsername() {
         return username;
     }
 
     /**
-     *
      * @param card the card chosen by the player
      */
     public void setCard(Card card) {
-        card = card;
+        this.card = card;
     }
 
     /**
-     *
      * @return the card the player is using
      */
-    public Card getCard(){
-    return null; //PLACEHOLDER
+    public Card getCard() {
+        return card;
     }
 
     /**
-     *
+     * @return the behaviour of the player for the current turn
+     */
+    public BehaviourManager getBehaviour() {
+        return behaviour;
+    }
+
+    /**
      * @param worker the worker to be added in the workersList
      */
     public void setWorker(Worker worker) {
@@ -53,15 +62,14 @@ public class Player {
     }
 
     /**
-     *
      * @param id the id of the worker to return
      * @return the worker of the id passed by parameter
      */
-    public Worker getWorker(Worker.IDs id){
+    public Worker getWorker(Worker.IDs id) {
         Worker w = null;
-        for (Worker x : workersList){
+        for (Worker x : workersList) {
             //needed method getWorkerID in class Worker
-            if (x.getWorkerID() == id){
+            if (x.getWorkerID() == id) {
                 w = x;
             }
         }
@@ -70,18 +78,18 @@ public class Player {
 
     /**
      * Set the color of the player
+     *
      * @param color the color representative of the player
      */
-    public void setColor (WorkerColors color){
-        this.color= color;
+    public void setColor(WorkerColors color) {
+        this.color = color;
     }
 
     /**
-     *
      * @return the color of the workers of the player
      */
     public WorkerColors getColor() {
-        WorkerColors colorCopy= color;
+        WorkerColors colorCopy = color;
         return colorCopy;
     }
 }
