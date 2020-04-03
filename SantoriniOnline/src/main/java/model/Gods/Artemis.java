13f8b21a853a219f73721b71452b5c8091d20ed1
Alessandro Lisi <10621058@polimi.it>
2020-04-03 17:20:38 +0200

    Added gods: Artemis, Demeter, Pan

--- SantoriniOnline/src/main/java/model/Gods/Artemis.java
+++ SantoriniOnline/src/main/java/model/Gods/Artemis.java
@@ -14,18 +14,21 @@ public class Artemis extends Card {
 
     /**
      * costructor, same as card but it fills name and descriptions
+     *
      * @param p the player that plays with the card
      */
     public Artemis(Player p) {
         super(p);
         name = "Artemis";
-        description = "Il tuo operatio può muoversi una volta di più, ma non retrocedendo alla casella di partenza";
+        description = "Your Build: Your Worker may build one additional time, but not on the same space.";
+        startingPosition = new int[2];
 
     }
 
     /**
      * Artemis overrides the move method
      * SPECIAL EFFECT: Artemis can move a second time without going back to starting position
+     *
      * @param worker   A worker of the actual player
      * @param desiredX X Position where the player wants to move the worker
      * @param desiredY Y Position where the player wants to move the worker
@@ -36,59 +39,57 @@ public class Artemis extends Card {
     @Override
     public void move(Worker worker, int desiredX, int desiredY, Island island) throws InvalidMovementException, WinningException {
 
-        if (playedBy.getBehaviour().getMovementsRemaining() > 0) { //se non mi hanno bloccato il movimento
 
-            //dove parte il mio worker
-            int actualX = worker.getPosition()[0];
-            int actualY = worker.getPosition()[1];
+        //dove parte il mio worker
+        int actualX = worker.getPosition()[0];
+        int actualY = worker.getPosition()[1];
 
-            //posso andare li?
-            if (!isValidDestination(actualX, actualY, desiredX, desiredY, island)) {
-                throw new InvalidMovementException("Invalid move for this worker");
-            }
-
-
-            //se non mi sono mai mosso mi salvo da dove parto
-            if (playedBy.getBehaviour().getMovementsRemaining() == 2) {
-                startingPosition = new int[2]; ;
-                startingPosition[0] = desiredX;
-                startingPosition[1] = desiredY;
-            }
-            //se è la seconda volta che mi muovo allora controllo che non sto tornando dove ero prima
-            else if (playedBy.getBehaviour().getMovementsRemaining() == 1) {
-                if (desiredX == startingPosition[0] && desiredY == startingPosition[1]) {
-                    throw new IllegalArgumentException("ARTEMIS: on the second movement you can't come back to starting position ");
-                }
-            }
-
-            //se posso allora mi sposto
-            island.moveWorker(worker, desiredX, desiredY);
-
-            if (!checkWorkerPosition(island, worker, desiredX, desiredY)) {
-                throw new InvalidMovementException("The move is valid but there was an error applying desired changes");
-            } else {
-                //Memorizzo l'altitudine del worker per poi controllare se è effettivamente salito
-                int oldAltitudeOfPlayer = island.getCellCluster(actualX, actualY).getCostructionHeight();
-                checkWin(island, desiredX, desiredY, oldAltitudeOfPlayer);
-            }
-
+        //Checks if movement is available and if I can move there
+        if (!isValidDestination(actualX, actualY, desiredX, desiredY, island)) {
+            throw new InvalidMovementException("Invalid move for this worker");
         }
+
+
+        //se non mi sono mai mosso mi salvo da dove parto
+        if (playedBy.getBehaviour().getMovementsRemaining() == 2) {
+            startingPosition = new int[2];
+            ;
+            startingPosition[0] = desiredX;
+            startingPosition[1] = desiredY;
+        }
+        //se è la seconda volta che mi muovo allora controllo che non sto tornando dove ero prima
+        else if (playedBy.getBehaviour().getMovementsRemaining() == 1) {
+            if (desiredX == startingPosition[0] && desiredY == startingPosition[1]) {
+                throw new IllegalArgumentException("ARTEMIS: on the second movement you can't come back to starting position ");
+            }
+        }
+
+        //se posso allora mi sposto
+        island.moveWorker(worker, desiredX, desiredY);
+
+        if (!checkWorkerPosition(island, worker, desiredX, desiredY)) {
+            throw new InvalidMovementException("The move is valid but there was an error applying desired changes");
+        } else {
+            //Memorizzo l'altitudine del worker per poi controllare se è effettivamente salito
+            int oldAltitudeOfPlayer = island.getCellCluster(actualX, actualY).getCostructionHeight();
+            checkWin(island, desiredX, desiredY, oldAltitudeOfPlayer);
+        }
+
     }
 
     //ARTEMIS can move two times in normal conditions
     @Override
     public void resetBehaviour() {
-        BehaviourManager b = playedBy.getBehaviour();
-        b.setBlockPlacementLeft(1);
-        b.setCanBuildDomeEverywhere(false);
-        b.setCanClimb(true);
-        b.setMovementsRemaining(2);
+        BehaviourManager behaviour = playedBy.getBehaviour();
+        behaviour.setBlockPlacementLeft(1);
+        behaviour.setMovementsRemaining(2);
+        behaviour.setCanClimb(true);
+        behaviour.setCanBuildDomeEverywhere(false);
         //convenzione
         startingPosition[0] = -1;
         startingPosition[1] = -1;
 
     }
-
 
 
 }
---
+++ SantoriniOnline/src/main/java/model/Gods/Pan.java
@@ -0,0 +1,28 @@
+package model.Gods;
+
+import model.Card;
+import model.Player;
+import model.exception.WinningException;
+import model.gamemap.CellCluster;
+import model.gamemap.Island;
+
+public class Pan extends Card {
+
+    public Pan(Player p) {
+        super(p);
+        name = "Pan";
+        description = "Win Condition: You also win if your Worker moves down two or more levels.";
+
+    }
+
+    @Override
+    protected void checkWin(Island island, int x, int y, int oldAltitudeOfPlayer) throws WinningException {
+        super.checkWin(island, x, y, oldAltitudeOfPlayer);
+        CellCluster cellCluster = island.getCellCluster(x, y);
+        int deltaH = oldAltitudeOfPlayer - cellCluster.getCostructionHeight();
+        System.out.println("DEBUG: PAN: worker is going down from " + oldAltitudeOfPlayer + " to " + cellCluster.getCostructionHeight() + ", delta is " + deltaH);
+        if (deltaH >= 2) {
+            throw new WinningException("Worker went down of two levels of more");
+        }
+    }
+}
