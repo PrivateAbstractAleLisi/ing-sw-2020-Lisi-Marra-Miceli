9b2e705e3a6c17336da61ba864a585b8d1a3199e
Alessandro Lisi <10621058@polimi.it>
2020-06-25 23:37:43 +0200

    added model 'Exception' package javadoc

--- SantoriniOnline/src/main/java/it/polimi/ingsw/psp58/exceptions/DefeatException.java
+++ SantoriniOnline/src/main/java/it/polimi/ingsw/psp58/exceptions/DefeatException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.psp58.exceptions;
 
+/**
+ * thrown when someone is defeated (For example there isn't a worker he can move)
+ */
 public class DefeatException extends Exception {
     public DefeatException (String playerDefeatedUsername) {
         super(playerDefeatedUsername);
--- SantoriniOnline/src/main/java/it/polimi/ingsw/psp58/exceptions/NotFreeRoomAvailableException.java
+++ SantoriniOnline/src/main/java/it/polimi/ingsw/psp58/exceptions/NotFreeRoomAvailableException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.psp58.exceptions;
 
+/**
+ * thrown when trying to add a player with no available rooms in the lobby
+ */
 public class NotFreeRoomAvailableException extends Exception {
     public NotFreeRoomAvailableException(String message) {
         super(message);
